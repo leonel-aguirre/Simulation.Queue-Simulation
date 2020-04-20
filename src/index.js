60c843f75509c72bd42c81c4ef58063e5de97a0e
NoisyApple <leonel.aguirre77@gmail.com>
2020-04-20 00:59:55 -0500

    Pug implemented, added multi range slider for frequency control

--- src/index.js
+++ src/index.js
@@ -6,10 +6,25 @@ let operations = require("./operations.json");
 let canvas;
 let res = 9;
 
+let isRunning = false;
+let timerSeconds = 0;
+
+//Top container elements.
+let playButton = document.querySelector("#play-button");
+let pauseButton = document.querySelector("#pause-button");
+let stopButton = document.querySelector("#stop-button");
+let timer = document.querySelector("#timer-label");
+
+//Left container elements.
 let meanSlider = document.querySelector("#mean-slider");
 let meanValue = document.querySelector("#mean-value");
 let devSlider = document.querySelector("#dev-slider");
 let devValue = document.querySelector("#dev-value");
+
+//Multi Slider.
+let mSlider = document.querySelector(".multi-slider");
+mSlider.setAttribute("amount", operations.length);
+let sliderLabels = new Array(operations.length);
 
 let sketch = (p) => {
   p.setup = () => {
@@ -17,13 +32,22 @@ let sketch = (p) => {
     canvas.parent("canvas");
     generateTable(operations);
     drawBellCurve(meanSlider.value, devSlider.value);
+
+    for (let i = 0; i < sliderLabels.length; i++)
+      sliderLabels[i] = document.querySelector(`#freq-val-${i}`);
+
+    initMultiSlider(mSlider, sliderLabels);
     document.querySelector(".highcharts-credits").style.display = "none";
-    setListeners();
+    setListeners(p);
   };
 
   p.draw = () => {
     p.background(200);
     drawGrid(p);
+
+    if (isRunning) {
+      updateTimer();
+    }
   };
 };
 
@@ -39,7 +63,8 @@ let drawGrid = (p) => {
 };
 
 let generateTable = (operations) => {
-  let tableHtml = "<thead><tr><th>Operacion</th><th>Tiempo</th></tr></thead>";
+  let tableHtml =
+    "<thead><tr><th>Operacion</th><th>Tiempo</th><th>f</th></tr></thead>";
 
   for (let i = 0; i < operations.length; i++) {
     tableHtml += "<tr>";
@@ -48,10 +73,11 @@ let generateTable = (operations) => {
       if (key == "seconds") value = secondsToTime(value);
       tableHtml += `<td>${value}</td>`;
     }
-    tableHtml += "</tr>";
+    tableHtml += `<td><div id=freq-val-${i} class='freq-data'><div class='color-indicator'></div><span>0%</span></div></td></tr>`;
   }
 
-  tableHtml += "<tfoot><tr><th>Operacion</th><th>Tiempo</th></tr></tfoot>";
+  tableHtml +=
+    "<tfoot><tr><th>Operacion</th><th>Tiempo</th><th>f</th></tr></tfoot>";
   let tableElement = document.createElement("table");
   tableElement.classList.add("operations-table");
   tableElement.innerHTML = tableHtml;
@@ -59,7 +85,20 @@ let generateTable = (operations) => {
   document.querySelector("#operations").appendChild(tableElement);
 };
 
-let setListeners = () => {
+let setListeners = (p) => {
+  //Top container elements.
+  playButton.addEventListener("click", () => {
+    console.log("PLAY BUTTON PRESSED");
+    isRunning = true;
+  });
+
+  pauseButton.addEventListener("click", () => {
+    isRunning = false;
+  });
+
+  stopButton.addEventListener("click", () => {});
+
+  //Left container elements.
   meanSlider.addEventListener("input", (e) => {
     drawBellCurve(e.target.value, devSlider.value);
     document.querySelector(".highcharts-credits").style.display = "none";
@@ -71,6 +110,18 @@ let setListeners = () => {
     document.querySelector(".highcharts-credits").style.display = "none";
     devValue.textContent = secondsToTime(devSlider.value);
   });
+
+  //KeyEvents.
+
+  document.body.addEventListener("keypress", (e) => {
+    if (e.keyCode == 32) {
+      isRunning = !isRunning;
+    }
+  });
+};
+
+let updateTimer = () => {
+  timer.textContent = secondsToTime(timerSeconds++);
 };
 
 let secondsToTime = (s) => {
@@ -142,12 +193,120 @@ const drawBellCurve = (mean, stdDev) => {
       },
       area: {
         enableMouseTracking: true,
-        color: "#333",
-        fillColor: "#3338",
+        color: "#F80",
+        fillColor: "#F808",
         zoneAxis: "x",
       },
     },
   });
 };
+
+function initMultiSlider(multiSlider, labels) {
+  console.log(labels);
+
+  let amount = multiSlider.getAttribute("amount");
+  let min = multiSlider.getAttribute("min");
+  let max = multiSlider.getAttribute("max");
+  let values = new Array(amount);
+  let freqText = new Array(amount);
+
+  for (let i = 0; i < labels.length; i++) {
+    let indicator = labels[i].querySelector(".color-indicator");
+    indicator.style.background = `hsl(${
+      (360 / labels.length) * (i + 1)
+    }deg, 100%, 70%)`;
+
+    freqText[i] = labels[i].querySelector("span");
+  }
+
+  let updateValues = (sliders) => {
+    let actualV = 0;
+
+    for (let i = 0; i < sliders.length; i++) {
+      let sliderV = sliders[i].value;
+      values[i] = sliderV - actualV;
+      actualV = sliderV;
+    }
+
+    values[amount - 1] = max - actualV;
+
+    multiSlider.setAttribute("values", values);
+
+    for (let i = 0; i < values.length; i++)
+      freqText[i].textContent = `${values[i]}%`;
+
+    // console.log(values)
+
+    let gradient = "to right, ";
+    let aValue = 0;
+    for (let i = 0; i < amount; i++) {
+      if (i == 0) {
+        aValue += values[i];
+        gradient += `hsl(${
+          (360 / amount) * (i + 1)
+        }deg, 100%, 70%) ${aValue}% , `;
+      } else if (i == amount - 1) {
+        gradient += `hsl(${
+          (360 / amount) * (i + 1)
+        }deg, 100%, 70%) ${aValue}%, `;
+        aValue += values[i];
+        gradient += `hsl(${(360 / amount) * (i + 1)}deg, 100%, 70%) ${aValue}%`;
+      } else {
+        gradient += `hsl(${
+          (360 / amount) * (i + 1)
+        }deg, 100%, 70%) ${aValue}%, `;
+        aValue += values[i];
+        gradient += `hsl(${
+          (360 / amount) * (i + 1)
+        }deg, 100%, 70%) ${aValue}%, `;
+      }
+    }
+
+    // console.log(gradient)
+
+    multiSlider.style.background = `linear-gradient(${gradient})`;
+  };
+
+  for (let i = 0; i < amount - 1; i++) {
+    let newSlider = document.createElement("input");
+    newSlider.type = "range";
+    newSlider.classList.add(".m-slider");
+    newSlider.min = min;
+    newSlider.max = max;
+    newSlider.setAttribute("value", ((max - min) / amount) * (i + 1));
+
+    newSlider.addEventListener("input", (e) => {
+      let parent = e.target.parentNode;
+      let siblings = parent.getElementsByTagName("input");
+
+      let slider = e.target;
+      let val = parseFloat(slider.value);
+      let leftLimit;
+      let rightLimit;
+
+      if (i == 0) {
+        leftLimit = min;
+        rightLimit = parseFloat(siblings[i + 1].value);
+      } else if (i == amount - 2) {
+        leftLimit = parseFloat(siblings[i - 1].value);
+        rightLimit = max;
+      } else {
+        leftLimit = parseFloat(siblings[i - 1].value);
+        rightLimit = parseFloat(siblings[i + 1].value);
+      }
+
+      if (val >= rightLimit) slider.value = rightLimit - 1;
+      if (val <= leftLimit) slider.value = leftLimit + 1;
+
+      console.log(leftLimit, slider.value, rightLimit);
+
+      updateValues(siblings);
+    });
+
+    multiSlider.appendChild(newSlider);
+  }
+
+  updateValues(multiSlider.getElementsByTagName("input"));
+}
 
 new p5(sketch);
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   resolve: {
-    extensions: [".js"]
+    extensions: [".js"],
   },
   context: __dirname,
   entry: {
-    app: ["./src/index.js", "./src/style.css"]
+    app: ["./src/index.js", "./src/style.css"],
   },
   output: {
     path:
       "C:\\Users\\Leonel\\Desktop\\Simulation Queue\\queues simulation\\build",
     filename: "app.js",
-    publicPath: "/build/"
+    publicPath: "/build/",
   },
   module: {
     rules: [
       {
+        test: /\.pug$/,
+        use: ["pug-loader"],
+      },
+      {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
+        loader: "babel-loader",
+      },
+    ],
   },
   devServer: {
     port: 8081,
-    inline: true
-  }
+    inline: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.pug",
+    }),
+  ],
 };
